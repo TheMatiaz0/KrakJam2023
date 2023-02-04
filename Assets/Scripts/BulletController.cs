c9b4b02dea65dad60be6b174fc2a6397a4fc0039
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-04 01:07:27 +0100

    Add bullet shoot full

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -8,11 +8,23 @@ public class BulletController : MonoBehaviour
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firePoint;
 
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletTimeToDisappear;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            var bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            var bullet = Instantiate(bulletPrefab, firePoint.position, this.transform.rotation);
+            var rb2D = bullet.GetComponent<Rigidbody2D>();
+            rb2D.AddForce(bullet.transform.right * bulletSpeed, ForceMode2D.Impulse);
+            StartCoroutine(DestroyBullet(bullet));
         }
+    }
+
+    private IEnumerator DestroyBullet(GameObject bullet)
+    {
+        yield return new WaitForSeconds(bulletTimeToDisappear);
+        Destroy(bullet);
     }
 }
