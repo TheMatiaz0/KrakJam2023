3bbba15df4bc0dd51ff7709efd1dcf5865852bd7
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-04 04:43:55 +0100

    Replace OnCollision to stay as update for hp taker

--- Assets/Scripts/CollisionHpTaker.cs
+++ Assets/Scripts/CollisionHpTaker.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 
 public class CollisionHpTaker : MonoBehaviour
 {
-    [SerializeField] private int damage;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float cooldown = 0.5f;
+
+    private Coroutine damageCoroutine;
     
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (damageCoroutine == null)
+        {
+            damageCoroutine = StartCoroutine(DamageWithDelay(col));
+        }
+    }
+
+    private IEnumerator DamageWithDelay(Collision2D col)
     {
         var colObj = col.gameObject;
         if (colObj.TryGetComponent<HpEntity>(out var entity) && colObj.GetComponent<PlayerInstance>() != null)
         {
             entity.Hp -= damage;
+            yield return new WaitForSeconds(cooldown);
         }
+
+        damageCoroutine = null;
     }
 }
