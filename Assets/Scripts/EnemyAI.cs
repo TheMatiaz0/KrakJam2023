6928852bc46271ac89be5b8b57d594dd972a01f3
PasiDisbanq <pasidisbanq@gmail.com>
2023-02-04 03:24:15 +0100

    Update EnemyAI.cs

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public Rigidbody2D rgbd;
+    public Transform PlayerTransform;
+    public float speed = 1;
+
+    private void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        
+        rgbd.transform.position += new Vector3(PlayerTransform.transform.position.x-rgbd.transform.position.x, 0, 0) * (Time.deltaTime * speed);
     }
 }
