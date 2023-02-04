062e9e38f9efc5638ae52d85735336e916a73880
Maticzpl <maticzpl06@gmail.com>
2023-02-04 11:34:18 +0100

    Animations

--- Assets/Scripts/OverlayFrames.cs
+++ Assets/Scripts/OverlayFrames.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OverlayFrames : MonoBehaviour
 {
     [SerializeField] private List<Sprite> frames;
+    [SerializeField] private List<Sprite> framesOverlaid;
     [SerializeField] public int index;
 
     private SpriteRenderer sprite;
@@ -16,6 +17,7 @@ public class OverlayFrames : MonoBehaviour
 
     void Update()
     {
-        sprite.material.SetTexture("_Overlay", frames[index].texture);
+        sprite.material.SetTexture("_BaseTexture", frames[index].texture);
+        sprite.material.SetTexture("_Overlay", framesOverlaid[index].texture);
     }
 }
