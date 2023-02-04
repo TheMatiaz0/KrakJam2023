a1bd296b2f60a10600388aa64343a9689a1996e9
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-04 03:26:45 +0100

    Add player singleton

--- Assets/Scripts/PlayerInstance.cs
+++ Assets/Scripts/PlayerInstance.cs
@@ -1,7 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInstance : MonoBehaviour
 {
+    public static PlayerInstance Current 
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        Current = this;
+    }
 }
