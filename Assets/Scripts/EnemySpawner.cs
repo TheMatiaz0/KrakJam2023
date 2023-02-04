744d4d884291992609ade7b8c7da1eb28ab949bd
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-04 05:53:32 +0100

    Start working on ranged enemy, improve Spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    private List<GameObject> spawnedEnemies;
+
+    private KeyCode[] keyCodes =
+    {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3,
+        KeyCode.Alpha4,
+        KeyCode.Alpha5,
+        KeyCode.Alpha6,
+        KeyCode.Alpha7,
+        KeyCode.Alpha8,
+        KeyCode.Alpha9,
+    };
+
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        for (int i = 0; i < keyCodes.Length; i++)
         {
-            var enemy = Instantiate(enemyPrefab, this.transform);
-            enemy.name = $"Enemy_{Random.Range(0, 9999)}";
+            if (Input.GetKeyDown(keyCodes[i]) && i < enemyPrefabs.Length)
+            {
+                var enemy = Instantiate(enemyPrefabs[i], this.transform);
+                enemy.gameObject.name = $"Enemy_{spawnedEnemies.Count + 1}";
+                spawnedEnemies.Add(enemy);
+            }
         }
     }
 }
