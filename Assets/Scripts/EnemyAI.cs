cc6b7e38a686acedd7415849bc0364e92dd92ff9
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-04 04:00:44 +0100

    Refactor EnemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,17 +6,23 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private Rigidbody2D rgbd;
+    private Rigidbody2D body;
     
     public float speed = 1;
+    public float jump = 2;
 
     private void Start()
     {
-        rgbd = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        rgbd.transform.position += new Vector3(PlayerInstance.Current.transform.position.x-rgbd.transform.position.x, 0, 0) * (Time.deltaTime * speed);
+        transform.position = Vector2.MoveTowards (transform.position, new Vector2(PlayerInstance.Current.transform.position.x, transform.position.y), speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // body.velocity = Vector2.up * jump;
     }
 }
