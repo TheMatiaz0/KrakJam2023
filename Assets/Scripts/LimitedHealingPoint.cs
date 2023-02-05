53826276b90f39512130c98f2161009fe0be73e8
Maticzpl <maticzpl06@gmail.com>
2023-02-05 12:07:14 +0100

    Healing stuff

--- Assets/Scripts/LimitedHealingPoint.cs
+++ Assets/Scripts/LimitedHealingPoint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class LimitedHealingPoint : MonoBehaviour
@@ -9,6 +10,7 @@ public class LimitedHealingPoint : MonoBehaviour
     [SerializeField] private float cooldown = 50f;
     [SerializeField] private float healRate = 1f;
     [SerializeField] private float refresh = 10f;
+    [SerializeField] private SpriteRenderer root;
 
     private Coroutine healCoroutine;
     private bool playerIn;
@@ -28,6 +30,9 @@ public class LimitedHealingPoint : MonoBehaviour
     {
         if (playerIn && healCoroutine == null)
             healCoroutine = StartCoroutine(HealWithCooldown(playerCol));
+        if(!playerIn)
+            root.material.DOFloat(0, "_Grow", 1);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -56,14 +61,20 @@ public class LimitedHealingPoint : MonoBehaviour
             if (capacity > 0)
             {
                 if (!sound.isPlaying)
+                {
+                    root.material.DOFloat(1, "_Grow", 2);
+                    
                     sound.Play();
-                
+                }
+
                 var change = healRate * (cooldown / 1000);
                 entity.Hp += change;
                 capacity -= change;
             }
             else
             {
+                root.material.DOFloat(0, "_Grow", 1);
+                
                 sound.Stop();
                 particles.Stop();
                 yield return new WaitForSeconds(refresh);
