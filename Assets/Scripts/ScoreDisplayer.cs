7f4e7d9a7a19ae5a4d4351ec887069eeccf872a7
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-05 11:18:36 +0100

    Add score animation

--- Assets/Scripts/ScoreDisplayer.cs
+++ Assets/Scripts/ScoreDisplayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,10 @@ public class ScoreDisplayer : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
     [SerializeField] private int scoreForKilling = 10;
+    [SerializeField] private float scoreScaleDuration = 6;
+    [SerializeField] private float scoreAnimationMultiplier = 1.2f;
+
+    private Vector2 cachedScale;
 
     private int _scorePoints;
     private int ScorePoints
@@ -16,12 +21,20 @@ public class ScoreDisplayer : MonoBehaviour
         set
         {
             _scorePoints = value;
+            scoreText.rectTransform.DOScale(
+                new Vector2(scoreText.rectTransform.localScale.x * scoreAnimationMultiplier, 
+                    scoreText.rectTransform.localScale.y * scoreAnimationMultiplier), 
+                scoreScaleDuration / 2).OnComplete(() =>
+            {
+                scoreText.rectTransform.DOScale(cachedScale , scoreScaleDuration / 2);
+            }).SetLink(this.gameObject);
             scoreText.text = value.ToString();
         }
     }
 
     private void Start()
     {
+        cachedScale = scoreText.rectTransform.localScale;
         ScorePoints = 0;
         HpEntity.OnEnemyDied += OnEnemyDied;
     }
