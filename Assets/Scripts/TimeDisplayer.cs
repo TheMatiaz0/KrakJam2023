e0c6a8e5d021af06ef66f8614ffbd341f589d638
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-02-05 10:15:27 +0100

    Fixes

--- Assets/Scripts/TimeDisplayer.cs
+++ Assets/Scripts/TimeDisplayer.cs
@@ -7,12 +7,11 @@ using UnityEngine.UI;
 public class TimeDisplayer : MonoBehaviour
 {
     [SerializeField] private Text timeText;
-    [SerializeField] private LevelController lvlController;
 
     private void Update()
     {
-        var p1 = TimeSpan.FromSeconds(lvlController.WinTimer);
-        var p2 = TimeSpan.FromMinutes(lvlController.WinTimerThreshold / 60);
+        var p1 = TimeSpan.FromSeconds(LevelController.Current.WinTimer);
+        var p2 = TimeSpan.FromMinutes(LevelController.Current.WinTimerThreshold / 60);
         var dt = (p2 - p1);
         if (dt.Seconds >= 0)
         {
